bb8943af5cf7a0a0b9db2f9ff614c7922e0711ae
Tan Tran <2188362+tant@users.noreply.github.com>
2025-07-06 14:34:20 +0700

    feat: Update mobile navigation component

--- components/NavMobile.tsx
+++ components/NavMobile.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet"
+import Nav from "./Nav"
+import Logo from "./Logo"
+import { AlignJustify } from "lucide-react"
+
 
 const NavMobile = () => {
   return (
-    <div>NavMobile</div>
+    <Sheet> 
+      <SheetTrigger asChild>
+        <AlignJustify className="cursor-pointer" />
+      </SheetTrigger>
+      <SheetContent>
+        <div className="flex flex-col items-center justify-center h-full py-8">
+          <div className="flex flex-col items-center gap-y-32">
+            <Logo />
+            <Nav
+              containerStyles='flex flex-col gap-y-6 items-center'
+              linkStyles='text-1xl font-semibold'
+              underlineStyles='' />
+
+          </div>
+        </div>
+
+      </SheetContent>
+    </Sheet>
+
   )
 }
 
